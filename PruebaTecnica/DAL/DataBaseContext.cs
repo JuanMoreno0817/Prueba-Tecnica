5befd72763a9f73b5c1c9383fd66268a6d9607fd
JuanMoreno0817 <juanelmorenito13@gmail.com>
2024-08-25 01:36:59 -0500

    Add Entitie, Packages of Nuget and DataBaseContext

---
+++ PruebaTecnica/DAL/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.DAL.Entities;
+
+namespace PruebaTecnica.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasIndex(p => p.ID).IsUnique();
+            modelBuilder.Entity<Person>().ToTable("Person");
+        }
+    }
+}
---
+++ PruebaTecnica/DAL/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PruebaTecnica.DAL.Entities
+{
+    public class Person
+    {
+        public Guid ID { get; set; }
+
+        [Display(Name = "Documento")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Identification { get; set; }
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Telefono")]
+        public string Phone { get; set; }
+    }
+}
